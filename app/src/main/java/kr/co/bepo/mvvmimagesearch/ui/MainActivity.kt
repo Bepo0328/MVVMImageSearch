e3a96f0f1ff73e30474c96cf75f9f7cc5bfb6cd7
Bepo0328 <82445568+Bepo0328@users.noreply.github.com>
2021-08-02 03:35:37 +0900

    MVVMImageSearch: DetailsFragment 구현

--- app/src/main/java/kr/co/bepo/mvvmimagesearch/ui/MainActivity.kt
+++ app/src/main/java/kr/co/bepo/mvvmimagesearch/ui/MainActivity.kt
@@ -2,16 +2,35 @@ package kr.co.bepo.mvvmimagesearch.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import dagger.hilt.android.AndroidEntryPoint
+import kr.co.bepo.mvvmimagesearch.R
 import kr.co.bepo.mvvmimagesearch.databinding.ActivityMainBinding
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private lateinit var navController: NavController
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_main) as NavHostFragment
+        navController = navHostFragment.findNavController()
+
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
--- app/src/main/java/kr/co/bepo/mvvmimagesearch/ui/details/DetailsFragment.kt
+++ app/src/main/java/kr/co/bepo/mvvmimagesearch/ui/details/DetailsFragment.kt
@@ -1,16 +1,31 @@
 package kr.co.bepo.mvvmimagesearch.ui.details
 
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.graphics.drawable.Drawable
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isGone
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
+import kr.co.bepo.mvvmimagesearch.R
 import kr.co.bepo.mvvmimagesearch.databinding.FragmentDetailsBinding
 
 class DetailsFragment : Fragment() {
 
     private var _binding: FragmentDetailsBinding? = null
     private val binding get() = _binding!!
+
+    private val args: DetailsFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,5 +37,54 @@ class DetailsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        initViews()
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun initViews() = with(binding) {
+        val photo = args.photo
+
+        Glide.with(imageView)
+            .load(photo.urls.full)
+            .error(R.drawable.ic_error)
+            .listener(object : RequestListener<Drawable> {
+                override fun onLoadFailed(
+                    e: GlideException?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    progressBar.isGone = true
+                    return false
+                }
+
+                override fun onResourceReady(
+                    resource: Drawable?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    dataSource: DataSource?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    progressBar.isGone = true
+                    textViewCreator.isVisible = true
+                    textViewDescription.isVisible = photo.description != null
+                    return false
+                }
+            })
+            .into(imageView)
+
+        textViewDescription.text = photo.description
+
+        val uri = Uri.parse(photo.user.attributionUrl)
+        val intent = Intent(Intent.ACTION_VIEW, uri)
+
+        textViewCreator.apply {
+            text = "Photo by ${photo.user.name} on Unsplash"
+            setOnClickListener {
+                context.startActivity(intent)
+            }
+            paint.isUnderlineText = true
+        }
     }
 }
